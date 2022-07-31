2334618dcdab712d478634c5798fefa3a7dbf32d
denglijuan <1422547140@qq.com>
2022-07-31 15:37:24 +0800

    refactor: 重构 UpdateClassification 类，利用 PartialType、减少代码冗余

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 路由前缀
   app.setGlobalPrefix('v1');
+
+  // 内置验证管道
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/module/books/classification/dto/update-classification.dto.ts
+++ src/module/books/classification/dto/update-classification.dto.ts
@@ -1,9 +1,5 @@
-import { IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateClassification } from './create-calssification.dto';
 
-export class UpdateClassification {
-  @IsString()
-  readonly type?: string;
-
-  @IsString()
-  readonly describe?: string;
-}
+// 将 CreateClassification 的所有属性都设置可选的，并且继承了所有验证规则
+export class UpdateClassification extends PartialType(CreateClassification) {}
