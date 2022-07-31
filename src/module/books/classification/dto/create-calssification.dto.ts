f443d562b1dc5d3c077e8305d9d5452f9c34f1d5
denglijuan <1422547140@qq.com>
2022-07-31 15:21:02 +0800

    feat: 给 dto 类属性加上校验

--- src/module/books/classification/dto/create-calssification.dto.ts
+++ src/module/books/classification/dto/create-calssification.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreateClassification {
+  @IsString()
   readonly type: string;
+
+  @IsString()
   readonly describe: string;
 }
--- src/module/books/classification/dto/update-classification.dto.ts
+++ src/module/books/classification/dto/update-classification.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class UpdateClassification {
+  @IsString()
   readonly type?: string;
+
+  @IsString()
   readonly describe?: string;
 }
