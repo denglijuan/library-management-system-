99120ec69ed5905a6182c2276f25bd989d098e64
denglijuan <1422547140@qq.com>
2022-08-16 20:28:45 +0800

    feat: 增加 swagger 配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './throw/http-exception.filter';
 
@@ -11,6 +12,17 @@ async function bootstrap() {
 
   // 全局注册错误过滤器
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  // swagger 生成文档
+  const options = new DocumentBuilder()
+    .setTitle('图书管理系统') // 标题
+    .setDescription('') // 描述
+    .setVersion('1.0.0') // 版本
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('v1', app, document);
 
   // 内置验证管道;
   app.useGlobalPipes(
